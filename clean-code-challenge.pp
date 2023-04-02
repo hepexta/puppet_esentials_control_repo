23bcb7863b2e12a3f9bbbeeab6be1fc0e2345d98
hepexta <hepexta@gmail.com>
2023-04-02 11:53:44 +0200

    Update clean-code-challenge.pp

--- clean-code-challenge.pp
+++ clean-code-challenge.pp
@@ -1,29 +1,19 @@
 class big_ugly_mess {
-  user { 'bob':
-    ensure     => present,
-    home       => '/var/www/bob_wide_web',
-    managehome => true,
-  }
-  file { '/var/www/bob_wide_web/index.html':
-    ensure => file,
-    source => 'puppet:///big_ugly_mess/bob_wide_web/index.html'
-  }
-  user { 'carol':
-    ensure     => present,
-    home       => '/var/www/unicorn_central',
-    managehome => true,
-  }
-  file { '/var/www/unicorn_central/index.html':
-    ensure => file,
-    source => 'puppet:///big_ugly_mess/unicorn_central/index.html'
-  }
-  user { 'alice':
-    ensure     => present,
-    home       => '/var/www/alice',
-    managehome => true,
-  }  
-  file { '/var/www/alice/index.html':
-    ensure => file,
-    source => 'puppet:///big_ugly_mess/alice/index.html'
+  $home_dir = '/var/www'
+  $puppet_base_path = 'puppet:///big_ugly_mess'
+  $user_data = {
+    'bob' => 'bob_wide_web',
+	'carol' => 'unicorn_central',
+	'alice' => 'alice',
+  }.each | $username, $maindir |{
+    user { $username:
+      ensure     => present,
+      home       => "${home_dir}/${maindir}",
+      managehome => true,
+    }
+    file { "${home_dir}/${maindir}/index.html":
+      ensure => file,
+      source =>"$puppet_base_path/${maindir}/index.html"
+    }
   }
 }
